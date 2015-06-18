88e5d7aa5b1d29519a238a2169cfff359a43bd02
BuildTools <unconfigured@null.spigotmc.org>
2015-06-18 20:11:28 -0300

    Update

--- UDP/src/com/udp/main/ClienteUDP.java
+++ UDP/src/com/udp/main/ClienteUDP.java
@@ -1,5 +1,43 @@
 package com.udp.main;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
 public class ClienteUDP {
+
+	public static void main(String[] args) throws Exception {
+				
+		Scanner input = new Scanner(System.in);
+	
+		DatagramSocket clienteSocket = new DatagramSocket();
+		
+		InetAddress endereçoIP = InetAddress.getByName("localhost");
+		byte[] enviaDados = new byte[1024];
+		byte[] recebeDados = new byte[1024];
+		
+		String dados = "renata";
+		
+		enviaDados = dados.getBytes();
+		
+		DatagramPacket enviaPacote = new DatagramPacket(enviaDados, enviaDados.length, endereçoIP, 9988);
+	
+		clienteSocket.send(enviaPacote);
+		
+		
+		
+		DatagramPacket recebePacote = new DatagramPacket(recebeDados, recebeDados.length);
+		
+		clienteSocket.receive(recebePacote);
+		
+		String resposta = new String(recebePacote.getData());
+		
+		System.out.println("Servidor: "+ resposta);
+		
+		
+		input.close();
+		clienteSocket.close();		
+	}
 
 }
--- UDP/src/com/udp/main/ServidorUDP.java
+++ UDP/src/com/udp/main/ServidorUDP.java
@@ -1,5 +1,49 @@
 package com.udp.main;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class ServidorUDP {
+
+	public static void main(String[] args) throws Exception {
+		
+		DatagramSocket ServidorSocket = new DatagramSocket(9988);
+		byte[] dadosRecebidos = new byte[1024];
+		byte[] dadosEnvidados = new byte[1024];
+		
+		while(true){
+			DatagramPacket pacoteRecebido = new DatagramPacket(dadosRecebidos, dadosRecebidos.length);
+			
+			ServidorSocket.receive(pacoteRecebido);
+			
+			InetAddress endereçoIP = pacoteRecebido.getAddress();
+			int porta = pacoteRecebido.getPort();
+			
+			String dadosTemp = new String(pacoteRecebido.getData());
+			String dadosToSend = "";
+			
+				if(dadosTemp.equals("guarapari")){
+					dadosToSend = "Buzios";
+				}else if(dadosTemp.equals("renata")){
+					dadosToSend = "Amor da Minha Vida";
+				}else if(dadosTemp.equals("bolacha")){
+					dadosToSend = "Biscoito";
+				}else if(dadosTemp.equals("traz um")){
+					dadosToSend = "Insenso pra mim velho, namoral!";
+				}else{
+					dadosToSend = "meu pai";
+				}			
+			
+			dadosEnvidados = dadosToSend.getBytes();
+		
+			DatagramPacket pacoteEnviado = new DatagramPacket(dadosEnvidados, dadosEnvidados.length, endereçoIP, porta);
+			
+			ServidorSocket.send(pacoteEnviado);
+			
+			System.out.println(dadosTemp);
+
+		}
+	}
 
 }
